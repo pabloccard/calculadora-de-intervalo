8f8f0917e2e4ce8e316fbb029610e3779a8e1669
pabloccard <pabloccard@gmail.com>
2022-04-14 10:59:01 -0300

    -

--- script.js
+++ script.js
@@ -56,11 +56,11 @@ ok3.addEventListener('click', ()=> {
 
 
 leftArrow.addEventListener('click', ()=> {
-    if(interval > 0) {
+    if(interval > 1) {
         interval --
         animation2(`slide`,`5rem`)
     }
-    
+  
 })
 
 rightArrow.addEventListener('click', ()=> {
@@ -74,9 +74,12 @@ function animation2(animation, translate) {
     if(interval > 0 && interval < 24) {
         intervalText.style.display = 'none'
         intervalText.innerText = `${interval} em ${interval} horas`
+        if(interval == 1) intervalText.innerText = `${interval} em ${interval} hora`
         intervalText.style.transform = `translateX(${translate})`
         intervalText.style.display = 'flex'
         intervalText.style.animationName = `${animation}`;
+        
+        
 
         setTimeout(()=>{
             intervalText.style.transform = 'translateX(0)'
@@ -98,21 +101,36 @@ function transform() {
 
 function calcular() {
     newTime = hours + Number(interval);
-    if(hours > 23) hours = hours - 24;
+    
+    if(newTime > 23) newTime = newTime - 24;
+
 }
 
 function reTransform() {
-    if(hours < 10) hours = '0' + hours
-    if(hours < 10) hours = '0' + hours
-    
+    interval = (interval == 1) ? interval + ' em ' + interval + ' hora' : interval + ' em ' + interval + ' horas'
+   
+    if(minutes < 10) minutes = '0' + minutes 
+    if(hours < 10) hours = '0' + hours 
     if(newTime < 10) newTime = '0' + newTime
-    if(minutes < 10) minutes = '0' + minutes
+    
+    if(newTime > 0 && newTime < 12) newTime = newTime + ':' + minutes + ' da manhã';
+    if(newTime >= 12 && newTime <= 18) newTime = newTime + ':' + minutes + ' da tarde';
+    if(newTime >= 18 || newTime == 0) newTime = newTime + ':' + minutes + ' da noite';
+
+    if(hours > 0 && hours < 12) hours = hours + ':' + minutes + ' da manhã,';
+    if(hours >= 12 && hours <= 18) hours = hours + ':' + minutes + ' da tarde,';
+    if(hours >= 18 || hours == 0) hours = hours + ':' + minutes + ' da noite,';
+    
+ 
+    
+     
+    
 }
 
 function textTransform() {
-    final.innerText = toptop + '! Agora você precisa tomar o seu remédio às '
-    new_time.innerText = " " + newTime + ":" + minutes 
-    console.log("você deve tomar seu remedio às " + hours + ":" + minutes)
+    final.innerText = toptop + '! Se você tomou, ou irá tomar o seu remédio às '+ 
+    hours + ' e o intervalo de tempo é de ' + interval + ', então a próxima vez que você irá tomar o seu remédio será às '
+    new_time.innerText = " " + newTime
 }
 
 function animation(element) {
